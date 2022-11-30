f74bd5db67e8bb0f19246f7f8cc584d4275e06a7
Anthony Dunford <trashzy21@gmail.com>
2022-11-30 15:04:53 +0000

    GenerateCsvs file

--- generateCsvs.py
+++ generateCsvs.py
@@ -1,4 +1,3 @@
-# imports csv module into this Python script (csv module part of Python's standard library - so you will have it if you have Python)
 from datetime import datetime
 import time
 import helpers.eloMath
@@ -18,16 +17,9 @@ csvList = [
 
 startingElo = 1000
 print("Starting Elo:", startingElo)
+
 kWeight = 20
-
-# for k in range(10, 30):
-#     seasons, teams = readCsvs(csvList)
-#     calculateElos(seasons, teams, startingElo, k)
-
-#     logValues, logTotal = logEvaluationForSeason(seasons[-1])
-#     #for logValue in logValues:
-#         #print(logValue,": ",logValues[logValue])
-#     print("k: ",k, "log: ",logTotal)
+print("Weight K:", kWeight)
 
 seasons, teams = readCsvs(csvList)
 calculateElos(seasons, teams, startingElo, kWeight)
