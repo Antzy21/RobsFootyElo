233c9d231011b6a36ddb09eeeb9208c94be37dd2
Anthony Dunford <trashzy21@gmail.com>
2022-11-30 15:06:47 +0000

    Fix calculate adjustment values

--- calculateAdjustmentValues.py
+++ calculateAdjustmentValues.py
@@ -6,19 +6,25 @@ from helpers.classes import *
 from helpers.manipulateData import *
 from math import exp
 
+
 csvList = [
-    "12_13Season.csv",
-    "13_14Season.csv",
-    "14_15Season.csv",
-    "15_16Season.csv",
-    "16_17Season.csv",
-    "17_18Season.csv",
-    #"18_19Season.csv",
+    "12_13.csv",
+    "13_14.csv",
+    "14_15.csv",
+    "15_16.csv",
+    "16_17.csv",
+    "17_18.csv",
+    "18_19.csv",
 ]
 
 startingElo = 1000
+print("Starting Elo:", startingElo)
+
+kWeight = 20
+print("Weight K:", kWeight)
+
 seasons, teams = readCsvs(csvList)
-calculateElos(seasons, teams, startingElo)
+calculateElos(seasons, teams, startingElo, kWeight)
 
 homeWins = 0
 awayWins = 0
