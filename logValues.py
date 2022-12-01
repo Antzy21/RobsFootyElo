a0131133c3967ce9db821ef02d9c3e1438773a66
Anthony Dunford <trashzy21@gmail.com>
2022-12-01 12:19:45 +0000

    LogValue ranges

--- logValues.py
+++ logValues.py
@@ -6,10 +6,10 @@ from helpers.classes import *
 from helpers.manipulateData import *
 
 csvList = [
-    "12_13.csv",
-    "13_14.csv",
-    "14_15.csv",
-    "15_16.csv",
+    #"12_13.csv",
+    #"13_14.csv",
+    #"14_15.csv",
+    #"15_16.csv",
     "16_17.csv",
     "17_18.csv",
     "18_19.csv",
@@ -18,15 +18,13 @@ csvList = [
 startingElo = 1000
 print("Starting Elo:", startingElo)
 
-kWeight = 20
-print("Weight K:", kWeight)
+for k in range(200,1000,20):
+    csvDicts = csvsToDictionary(csvList)
+    seasons, teams = buildSeasonsAndTeams(csvDicts)
+    calculateElos(seasons, teams, startingElo, k)
 
-csvDicts = csvsToDictionary(csvList)
-seasons, teams = buildSeasonsAndTeams(csvDicts)
-calculateElos(seasons, teams, startingElo, kWeight)
-
-logValues, logTotal = logEvaluationForSeason(seasons[-1])
-for logValue in logValues:
-    print(logValue,": ",logValues[logValue])
+    logValues, logTotal = logEvaluationForSeason(seasons[-1])
+    for logValue in logValues:
+        pass#print(logValue,": ",logValues[logValue])
     
-print("Log total: ", logTotal)
+    print("Weight K:", k, " - Log total: ", logTotal)
