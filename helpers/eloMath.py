87c702738c134e18d1603e5d76ee1b2ec1daf385
Anthony Dunford <trashzy21@gmail.com>
2022-12-01 12:42:02 +0000

    Get goalDif elo calculation setup

--- helpers/eloMath.py
+++ helpers/eloMath.py
@@ -40,31 +40,21 @@ def eloCalculationWithGoalDif(
     t1,
     t2,
     result : tuple[int, int],
-    weightK = 20
+    weightK = 20,
+    weightGd = 20,
     ):
-    
+
+    t1, t2 = eloCalculation(t1, t2, result, weightK)
+
     # Step 1
-    T1 = 10 ** (t1/400)
-    T2 = 10 ** (t2/400)
+    E1 = (T1-T2)/weightGd
+    E2 = (T2-T1)/weightGd
     # Step 2
-    E1 = T1/(T1+T2)
-    E2 = T2/(T1+T2)
-    # Step 3
-    if result[0] > result[1]:
-        # if team1 wins (scores more goals)
-        S1 = 1
-        S2 = 0
-    elif result[0] < result[1]:
-        # if team2 wins (scores more goals)
-        S1 = 0
-        S2 = 1
-    elif result[0] == result[1]:
-        # if draw (scores same amount of goals)
-        S1 = 1/2
-        S2 = 1/2
+    S1 = result[0]-result[1]
+    S2 = result[1]-result[0]
     # Step 4
-    t1_ = round(t1 + weightK * (S1 - E1))
-    t2_ = round(t2 + weightK * (S2 - E2))
+    t1_ = round(t1 + weightGd * (S1 - E1))
+    t2_ = round(t2 + weightGd * (S2 - E2))
     
     return(t1_, t2_)
 
