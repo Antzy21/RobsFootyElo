f519e86f7bb2bc715a3d70f292eb2d3798985c8e
Anthony Dunford <trashzy21@gmail.com>
2022-12-01 12:25:27 +0000

    Find best log

--- logValues.py
+++ logValues.py
@@ -6,10 +6,10 @@ from helpers.classes import *
 from helpers.manipulateData import *
 
 csvList = [
-    #"12_13.csv",
-    #"13_14.csv",
-    #"14_15.csv",
-    #"15_16.csv",
+    "12_13.csv",
+    "13_14.csv",
+    "14_15.csv",
+    "15_16.csv",
     "16_17.csv",
     "17_18.csv",
     "18_19.csv",
@@ -18,7 +18,9 @@ csvList = [
 startingElo = 1000
 print("Starting Elo:", startingElo)
 
-for k in range(200,1000,20):
+bestLog = -1000000
+
+for k in range(0,1000,10):
     csvDicts = csvsToDictionary(csvList)
     seasons, teams = buildSeasonsAndTeams(csvDicts)
     calculateElos(seasons, teams, startingElo, k)
@@ -27,4 +29,9 @@ for k in range(200,1000,20):
     for logValue in logValues:
         pass#print(logValue,": ",logValues[logValue])
     
-    print("Weight K:", k, " - Log total: ", logTotal)
+    #print("Weight K:", k, " - Log total: ", logTotal)
+    if logTotal > bestLog:
+        bestLog = logTotal
+        optimumK = k
+
+print("optimum K:", optimumK, " - Log total: ", bestLog)
