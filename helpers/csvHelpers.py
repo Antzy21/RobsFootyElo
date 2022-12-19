7bb22580add490a34bb123b636dc8a9b7666856f
Anthony Dunford <Anthony@Dunford.uk.com>
2022-12-19 22:22:25 +0000

    Add postbetCapital to csv

--- helpers/csvHelpers.py
+++ helpers/csvHelpers.py
@@ -119,7 +119,7 @@ def constructGamesCsv(
         line += ",Home Win Prob,Draw Prob,Away Win Prob"
         line += ",Bet Home,Bet Draw,Bet Away"
         line += ",Bet Size Home,Bet Size Draw,Bet Size Away"
-        line += ",Net Change"
+        line += ",Net Change,Capital"
         outputFile.write(line+"\n")
         if printLine:
             print(line)
@@ -134,7 +134,7 @@ def constructGamesCsv(
                 line += f",{game.probabilities.homeWin},{game.probabilities.draw},{game.probabilities.awayWin}"
                 line += f",{game.bet.homekelly},{game.bet.drawkelly},{game.bet.awaykelly}"
                 line += f",{game.bet.homeKellyBetSize},{game.bet.drawKellyBetSize},{game.bet.awayKellyBetSize}"
-                line += f",{game.bet.netChange}"
+                line += f",{game.bet.netChange},{game.bet.postBetCapital}"
                 if printLine:
                     print(line)
                 line += "\n"
