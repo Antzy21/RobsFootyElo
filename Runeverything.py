6e24662a6faf02008f26f01b11d242857bfb838d
Bertborr <119080682+Bertborr@users.noreply.github.com>
2022-12-24 12:58:40 +0000

    new additions

--- Runeverything.py
+++ Runeverything.py
@@ -4,6 +4,7 @@ from generateCsvs import runallcsvs
 from datetime import datetime
 from Evaluation import printLogScore
 from calculateAdjustmentValues import calculateadjustment
+from helpers.eloMath import eloPrediction
 
 startingElo = 1000
 print("Starting Elo:", startingElo)
@@ -17,12 +18,15 @@ seasons, teams = parseSeasonsAndTeams(csvDicts)
 lowestElo = startingElo
 capital = 10
 for season in seasons:
-    capital = season.playMatches(kWeight, defaultElo=lowestElo, capital=capital, betAfterDate=datetime(2016,6,1))
+    capital = season.playMatches(kWeight, defaultElo=lowestElo, capital=capital, betAfterDate=datetime(2005,6,1))
     lowestElo = getLowestElo(teams)
     #input(f"Next Season: {season.name}")
 
-runallcsvs(seasons,teams)
+#runallcsvs(seasons,teams)
 
 #printLogScore(seasons)
 
 #calculateadjustment(seasons)
+
+h, d, a = eloPrediction(teams["Chelsea"].elo, teams["Chelsea"].elo)
+print(h, d, a)
